9a82267bb68625dcebb630ec4a8a50f945c68185
yonefactory <yonefactory@gmail.com>
2025-03-02 00:46:15 +0900

    Update test_mode.py

--- scripts/test_mode.py
+++ scripts/test_mode.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import config
 from generate_messages import create_message
 from send_telegram import send_message
 
